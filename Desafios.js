2c0bf9a28d93a9ef277b203ab2557f8ea03fd38c
Luiza Timporini <luizatimporinifrazao@gmail.com>
2026-02-09 15:49:06 -0300

    Add files via upload

---
+++ Desafios.js
@@ -0,0 +1,63 @@
+//desafio 1
+let meuNome = "Luiza Timporini Frazão";
+var idade = 16;
+console.log(meuNome , idade);
+
+//desafio 2
+// let comidaFavorita="Lasanha";
+// comidaFavorita="tortei";
+
+// const paisNascido="italia"
+
+// const paisNascido="brasil";
+
+
+//desafio 3
+let nome = "Luiza Timporini Frazão";
+var idade = 16;
+var cidade = "Guarulhos";
+console.log(meuNome , idade, cidade);
+
+
+//desafio 4
+
+
+var curso = "Design Grafico";
+console.log(curso);
+curso = "Programação";
+console.log(curso);
+
+
+//desafio 5
+
+// const anoAtual=2026;
+// anoAtual=2027;
+
+//desafio 6
+
+var nomezinho="luiza";
+var sobrenome="timporini";
+console.log(nomezinho,sobrenome);
+
+//desafio 7
+var user = "l.timporini";
+var email = "luiza.t.frazao@aluno.senai.br";
+var age = 16;
+console.log(user, email, age)
+
+//desafio 8
+var num1 = 2;
+var num2 = 4;
+console.log(num1 , num2)
+
+//desafio 9
+var nota1 = 9
+var nota2 = 9
+var nota3 = 10
+console.log(nota1, nota2, nota3)
+
+
+//desafio 10
+var produto= "sabão"
+var preco= 8
+console.log(produto , preco)
